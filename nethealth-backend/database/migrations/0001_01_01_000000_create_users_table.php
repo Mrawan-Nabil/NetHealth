6e7f4dcbdaf381aa0fa73f2a645b43570111791d
Mariam Mansour <mm9360487@gmail.com>
2026-02-28 13:10:00 +0200

    add professional_title, syndicate_id, and consultation_fee to doctor , handle Request and controller

--- nethealth-backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ nethealth-backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('password');
             $table->string('role');
 
+
             $table->boolean('is_verified')->default(false);
             $table->string('account_status')->default(AccountStatus::Pending->value);
             $table->timestamp('created_at')->useCurrent();
