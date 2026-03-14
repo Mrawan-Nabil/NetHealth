e4ebfb13a2c88a573ccb5902910c8cc4febd36f5
Mrawan Nabil <mrawannabil2005@gmail.com>
2026-03-14 08:16:26 +0200

    added most of factories and DtaBaseSeeder

--- nethealth-backend/database/factories/AppointmentFactory.php
+++ nethealth-backend/database/factories/AppointmentFactory.php
@@ -11,7 +11,7 @@ use App\Models\Patient;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Appointment>
+ * @extends Factory<Appointment>
  */
 class AppointmentFactory extends Factory
 {
